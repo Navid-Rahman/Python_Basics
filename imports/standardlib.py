31b6d261143175d52058819e4e2996da535a451f
Navid Rahman <navidrahman92@gmail.com>
2026-05-11 19:11:59 +0600

    Blah Blah Blah

---
+++ imports/standardlib.py
@@ -0,0 +1,10 @@
+import random
+import math
+
+types_of_drinks = ["coffee", "tea", "juice", "water", "soda"]
+print(random.choice(types_of_drinks))
+
+print((random.randint(1,30)))
+
+square_root = math.sqrt(64)
+print(square_root)
