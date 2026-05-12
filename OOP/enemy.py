8efd1335362f2e9d270f1e14d1e6aeb47d14d511
Navid Rahman <navidrahman92@gmail.com>
2026-05-13 01:21:22 +0600

    Lots of blah blah with python oops - encapsulation

--- OOP/enemy.py
+++ OOP/enemy.py
@@ -4,7 +4,7 @@ class Enemy:
     attack_damage: int
 
     def __init__(self, type_of_enemy, health_points, attack_damage):
-        self.type_of_enemy = type_of_enemy
+        self.__type_of_enemy = type_of_enemy
         self.health_points = health_points
         self.attack_damage = attack_damage
 
@@ -16,3 +16,6 @@ class Enemy:
 
     def attack(self):
         print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage.')
+
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
--- OOP/main.py
+++ OOP/main.py
@@ -12,5 +12,4 @@ yeti = Enemy(
     attack_damage=10
 )
 
-# print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do an attack of {enemy.attack_damage} damage.')
-yeti.talk()
+print(zombie.get_type_of_enemy())
